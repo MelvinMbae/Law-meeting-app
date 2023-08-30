20587cba56fec6b068c54a921b23f6f1fae76a5a
LUCY <wangecikariu.luc@gmail.com>
2023-08-30 23:17:59 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
-import Meetings from './Components/meeting';
+import Meeting from '../Components/Meeting';
+import React from 'react';
+import Header from '../Components/Header';
+import MeetingForm from '../Components/MeetingForm';
+import MeetingList from '../Data/MeetingList';
+
 
 function App() {
   return (
     <div className="App">
-      <Meetings />
+      <Meeting Description="This Meeting is for Employees to Talk about the Ruling." Completed Task="Confirm if all Tasks have been Completed"/>
+      <Header title ="Ruling by Justice Maraga for Safaricom vs Airtel"/>
+      <MeetingList />
+      <MeetingForm Form="Feedback Form" Tasks="Tick where Appropriate"/>
     </div>
   );
 }
